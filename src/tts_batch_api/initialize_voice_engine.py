8bcf57a0283512706270eaf0340684b051025d1f
stkr22 <checkout@github.com>
2024-03-02 15:09:41 +0000

    fixing issues if current directory is not writable falling back to home directory;

--- src/tts_batch_api/initialize_voice_engine.py
+++ src/tts_batch_api/initialize_voice_engine.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 from typing import Any
 
@@ -5,8 +6,19 @@ import piper  # ignore
 from piper import download as piper_download
 
 
+def get_writable_directory() -> pathlib.Path:
+    # Check if the current directory is writable
+    current_directory = pathlib.Path(".")
+    if os.access(current_directory, os.W_OK):
+        return current_directory
+    else:
+        # Fallback to the user's home directory if the current directory is not writable
+        home_directory = pathlib.Path.home()
+        return home_directory
+
+
 def initialize_voice_engine(model: str) -> piper.PiperVoice:
-    download_dir = pathlib.Path.cwd()
+    download_dir = get_writable_directory()
     data_dir = [download_dir]
     model_path = pathlib.Path(model)
     if not model_path.exists():
@@ -22,5 +34,4 @@ def initialize_voice_engine(model: str) -> piper.PiperVoice:
         voices_info.update(aliases_info)
         piper_download.ensure_voice_exists(model, data_dir, download_dir, voices_info)
         model, config = piper_download.find_voice(model, data_dir)
-
     return piper.PiperVoice.load(model, config_path=config)
